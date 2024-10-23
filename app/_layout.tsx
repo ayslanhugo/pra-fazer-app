bb6fd7311d0f6ab19f2829a45a0e92284d5558d9
Ayslan Hugo Lúcio dos Santos <222.14.063@EDU.gruposete.local>
2024-10-22 22:42:44 -0300

    Implementações de Telas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,20 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack
+      screenOptions={{
+            headerStyle: {backgroundColor: "#F60" },
+            headerShadowVisible: false
+      }} >
+          
+      <Stack.Screen name="index" options={{
+        headerShown: false
+      }}/>
+
+      <Stack.Screen name="user_create" options={{
+        headerTitle: ''
+      }}/>
+
     </Stack>
   );
 }
